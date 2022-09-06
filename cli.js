a67fd93a1c7ce08d7c3522c713cdef3f2e54d837
Saul Johnson <saul.a.johnson@gmail.com>
2022-09-06 13:57:27 +0200

    Support prerelease and build versioning

--- cli.js
+++ cli.js
@@ -12,7 +12,7 @@ import chalk from 'chalk';
 /**
  * The location of the package.json file on disk.
  */
-const PAckAGE_JSON_PATH = './package.json';
+const PACKAGE_JSON_PATH = './package.json';
 
 /**
  * Whether or not the application is in quiet mode.
@@ -91,9 +91,15 @@ function fatal(msg, exitCode = 1) {
       --major-tag           The major version bump tag to search for in commit messages (default: '[major]')
       --minor-tag           The minor version bump tag to search for in commit messages (default: '[minor]')
       --patch-tag           The patch version bump tag to search for in commit messages (default: '[patch]')
+      --prerelease-tag      The prerelease version bump tag to search for in commit messages (default: '[prerelease]')
+      --build-tag           The build version bump tag to search for in commit messages (default: '[build]')
+      --prerelease-token    The prerelease token to prepend to the prerelease version (default: 'prerelease')
+      --build-token         The build token to prepend to the build version (default: '')
       --major               Specifies that a major version bump should be performed (default: false)
       --minor               Specifies that a minor version bump should be performed (default: false)
       --patch               Specifies that a patch version bump should be performed (default: true)
+      --prerelease          Specifies that a prerelease version bump should be performed (default: false)
+      --build               Specifies that a build version bump should be performed (default: false)
       --dry                 Specifies that the bumped version should not be written to disk (default: false)
       --quiet               Suppresses informational output (default: false)
     Boolean options can be inverted by prefixing '--no-' (e.g. '--no-patch').
@@ -121,6 +127,22 @@ function fatal(msg, exitCode = 1) {
                 type: 'string',
                 default: '[patch]',
             },
+            prereleaseTag: {
+                type: 'string',
+                default: '[prerelease]',
+            },
+            buildTag: {
+                type: 'string',
+                default: '[patch]',
+            },
+            prereleaseToken: {
+                type: 'string',
+                default: 'prerelease',
+            },
+            buildToken: {
+                type: 'string',
+                default: '',
+            },
             major: {
                 type: 'boolean',
             },
@@ -130,6 +152,12 @@ function fatal(msg, exitCode = 1) {
             patch: {
                 type: 'boolean',
                 default: true,
+            },
+            prerelease: {
+                type: 'boolean',
+            },
+            build: {
+                type: 'boolean',
             },
             dry: {
                 type: 'boolean',
@@ -146,7 +174,7 @@ function fatal(msg, exitCode = 1) {
     // Load raw package data.
     let rawPackageData;
     try {
-        rawPackageData = await readFileText(PAckAGE_JSON_PATH);
+        rawPackageData = await readFileText(PACKAGE_JSON_PATH);
     } catch (e) {
         fatal('Could not read package.json file.');
     }
@@ -178,13 +206,23 @@ function fatal(msg, exitCode = 1) {
         logInfo('Performing a patch version bump.');
     }
 
+    // Prerelease and build versions can be bumped independently.
+    if (cli.flags.prerelease || cli.flags.commitMessage.includes(cli.flags.prereleaseTag)) {
+        currentVersion.inc('prerelease', cli.flags.prereleaseToken);
+        logInfo('Performing a prerelease version bump.');
+    }
+    if (cli.flags.build || cli.flags.commitMessage.includes(cli.flags.buildTag)) {
+        currentVersion.inc('build', cli.flags.buildToken);
+        logInfo('Performing a build version bump.');
+    }
+
     // Log version bump.
     console.log(`${packageData.version} -> ${currentVersion.toString()}`);
 
     // If dry flag not provided, write new version to package.json file.
     if (!cli.flags.dry) {
         packageData.version = currentVersion.toString();
-        await writeFileText(PAckAGE_JSON_PATH, JSON.stringify(packageData, null, 2));
+        await writeFileText(PACKAGE_JSON_PATH, JSON.stringify(packageData, null, 2));
         logInfo('Version change was written to disk.');
     } else {
         logInfo('Version change was not written to disk because the --dry flag was passed.');
